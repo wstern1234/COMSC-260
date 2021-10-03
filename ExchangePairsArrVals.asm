91d37b4c95ba29e5ca7c88428a9aedf93c4500f4
wstern1234 <58192563+wstern1234@users.noreply.github.com>
2021-10-02 23:12:18 -0700

    Add files via upload

---
+++ ExchangePairsArrVals.asm
@@ -0,0 +1,25 @@
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+.data
+array dword 1,2,3,4,5,6,7,8
+.code
+main proc
+  mov esi, OFFSET array
+  mov ecx, LENGTHOF array -1
+ 
+myLoop:
+
+  MOV eax,[esi]
+  XCHG eax,[esi+4]
+  MOV [esi],eax
+
+  add esi, TYPE array
+  add esi, TYPE array
+
+loop myLoop
+
+  invoke ExitProcess,0
+main endp
+end main
